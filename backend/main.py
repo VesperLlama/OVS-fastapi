5cc6e94005379fab93aec8c0ca36b31691719593
VesperLlama <github@vesperllama.anonaddy.com>
2023-11-29 15:33:32 +0530

    Updates

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,7 @@ from pymongo import MongoClient
 from fastapi.middleware.cors import CORSMiddleware
 from models.electionSchema import registerSchema, loginSchema, voteSchema
 
+
 app = FastAPI()
 load_dotenv()
 app.add_middleware(
